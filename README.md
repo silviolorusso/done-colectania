06542df3f4d0ddfa66fa199e5c75c7888a97b5f4
Silvio <s@silviolorusso.com>
2017-11-28 16:54:14 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-# done-colectania
+### done-colectania
+
+## Premise: Digital Publishing is Easy
+
+Publishing, both for print and screen, is easy. And there are many ways to do it. Consider this very Google Doc, it took me a few seconds to create. Adding text and images, formatting it and exporting it as a PDF or as a document accessible online is not rocket science. Craig Mod evocates the speed and ease of contemporary publishing very lucidly when he speaks of mass indie:  
+
+Mass Indie is the zine publishing of web publishing. The everyperson indie. Godaddy a domain, snag a Tumblr, fiddle a DNS and Go Go Go. Don’t have eight bucks? Skip the domain and jump straight to Go Go Go. It’s right there and it’s faster than a Xerox at Kinkos. Don’t like Tumblr? Ghost it up. Livejournal’s still a thing. Wattpad welcomes all. Geo-plaster at hi.co. Kindle Single it and give it away. Toss it on Scribd. Pastebin the notion. Splatter your post across twenty tweets. Heck, Google Doc it. The Web Is Here For You To Use. Post to multiple platforms. Pledge allegiance to no one. You don’t owe ’em nuttin’. Everybody Minecraft — stake your claim. Then restake it again tomorrow. The land’s wide open and there’s always more IPv6 to go around.
+
+## Goal: Make Digital Publishing Challenging Again
+
+Why making another publishing tool that mimics the functions of the already simple and effective tools available? We have already plenty of  generalist tools and workflows, and they work just fine. 
+
+So, why not making publishing challenging by defining some obstacles, limitations, etc that need to be overcome by the publisher/user? What if publishing had obstacles? What if some friction in the publishing process make the publication more rewarding? What if we apply the IKEA principle to publishing?
+
+### Features
 
 - purely image zine, set the textual context and title at the beginning
 - Add a bit of time each time you add asset
---
+++ old.js
@@ -0,0 +1,165 @@
+// --- M-V-C
+var model = {
+  // all our states
+  time: new Date().getTime(),
+  expired: false
+};
+
+var time, duration = 10000;
+var expired = false;
+model = [
+  duration,
+  time
+];
+
+function updateView(input) {
+  // --- VIEWS
+  timer();
+  if ( input ) {
+    critic();
+  }
+  // expiration    
+  if (time < 0) {
+    clearInterval(x);
+    document.getElementById("counter").innerHTML = "EXPIRED";
+    expired = true;
+
+    // call to all views
+    drag();
+  } else {
+    seconds = time/1000;
+    document.getElementById("counter").innerHTML = seconds.toFixed(2) + " seconds left!";
+  }
+}
+
+function controller(input) {
+  updateView(input);
+}
+
+
+updateView();
+
+// --- CRITIC
+
+function critic() {
+  alert('Make this image bigger pls!');
+}
+
+
+// --- TIMER
+
+function timer() {
+  var x = setInterval(function() {
+    time = time - 10;
+  }, 10);
+  controller();
+}
+
+
+// --- FILE DROP
+
+var holder = document.getElementById('page')
+
+if (typeof window.FileReader === 'undefined') {
+  console.log('fail');
+} else {
+  console.log('success');
+}
+ 
+holder.ondragover = function () { this.className = 'hover'; return false; };
+holder.ondragend = function () { this.className = ''; return false; };
+holder.ondrop = function (e) {
+  this.className = '';
+  e.preventDefault();
+
+  if (expired == false) {
+    var file = e.dataTransfer.files[0], 
+        reader = new FileReader();
+    reader.onload = function (event) {
+      console.log(event.target);
+      var img = document.createElement("img"); 
+      img.className += " draggable"; 
+      img.src = event.target.result;
+      holder.appendChild(img);
+
+      controller(event.target.result);
+
+    };
+    console.log(file);
+    reader.readAsDataURL(file);
+  }
+
+  return false;
+};
+
+// --- DRAG 
+interact('.draggable')
+  .draggable({
+    onmove: window.dragMoveListener,
+    restrict: {
+      restriction: 'parent',
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    },
+  })
+  .resizable({
+    // resize from all edges and corners
+    edges: { left: true, right: true, bottom: true, top: true },
+
+    // keep the edges inside the parent
+    restrictEdges: {
+      outer: 'parent',
+      endOnly: true,
+    },
+
+    inertia: true,
+  })
+  .on('resizemove', function (event) {
+    var target = event.target,
+        x = (parseFloat(target.getAttribute('data-x')) || 0),
+        y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+    // update the element's style
+    target.style.width  = event.rect.width + 'px';
+    target.style.height = event.rect.height + 'px';
+
+    // translate when resizing from top or left edges
+    x += event.deltaRect.left;
+    y += event.deltaRect.top;
+
+    target.style.webkitTransform = target.style.transform =
+        'translate(' + x + 'px,' + y + 'px)';
+
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+    target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height);
+  });
+
+  function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
+
+  // this is used later in the resizing and gesture demos
+  window.dragMoveListener = dragMoveListener;
+
+// target elements with the "draggable" class
+function drag() {
+  if (expired == true) {
+    var elems = document.querySelectorAll(".draggable");
+    [].forEach.call(elems, function(el) {
+      el.classList.remove("draggable");
+    });
+  }
+}
+drag();
