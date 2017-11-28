91f447c1fe3e1223ad77a3d6a4709ae7599ad7ac
Stef Kors <stef.kors@gmail.com>
2017-11-28 16:06:49 +0100

    MVC setup + globaltimer

--- main.js
+++ main.js
@@ -1,25 +1,54 @@
 // --- M-V-C
 var model = {
-  // all our states
-  time: new Date().getTime(),
-  timeLeft: 10000,
-  expired: false
+	// all our states
+	time: new Date().getTime(),
+	timeLeft: 10000,
+	expired: false
 };
 
-function controller(input) {
-  // model.img.append(input)
-  if (input == "img") {
-    timeLeft = timeLeft + 1000;
-  }
+timer();
+
+
+function controller(model) {
+	// model.img.append(input)
+	// if (input == "img") {
+	// 	timeLeft = timeLeft + 1000;
+	// }
+	if (model.timeLeft > 0) {
+		showTime(model)
+	}
+	else {
+		model.expired == true;
+		showExpired();
+	}
+
 }
 
-// --- TIMER
+
+
+
+// --- CONTROLLER
 
 function timer() {
-  var x = setInterval(function() {
-    time = time - 10;
-  }, 10);
-  controller();
+	var x = setInterval(function() {
+		model.timeLeft = model.timeLeft - 10;
+		controller(model);
+	}, 10);
+}
+
+
+
+
+
+// --- VIEW
+
+function showTime(model) {
+	seconds = model.timeLeft / 1000;
+	document.getElementById("counter").innerHTML = seconds.toFixed(2) + " seconds left!";
+}
+
+function showExpired() {
+	document.getElementById("counter").innerHTML = "expired!";
 }
 
 // -------------------------------------
@@ -27,7 +56,7 @@ function timer() {
 // --- CRITIC
 
 function critic() {
-  alert('Make this image bigger pls!');
+	alert('Make this image bigger pls!');
 }
 
 // --- FILE DROP
@@ -35,105 +64,111 @@ function critic() {
 var holder = document.getElementById('page')
 
 if (typeof window.FileReader === 'undefined') {
-  console.log('fail');
-} else {
-  console.log('success');
+	console.log('fail');
 }
- 
-holder.ondragover = function () { this.className = 'hover'; return false; };
-holder.ondragend = function () { this.className = ''; return false; };
-holder.ondrop = function (e) {
-  this.className = '';
-  e.preventDefault();
+else {
+	console.log('success');
+}
 
-  if (expired == false) {
-    var file = e.dataTransfer.files[0], 
-        reader = new FileReader();
-    reader.onload = function (event) {
-      console.log(event.target);
-      var img = document.createElement("img"); 
-      img.className += " draggable"; 
-      img.src = event.target.result;
-      holder.appendChild(img);
+holder.ondragover = function() {
+	this.className = 'hover';
+	return false;
+};
+holder.ondragend = function() {
+	this.className = '';
+	return false;
+};
+holder.ondrop = function(e) {
+	this.className = '';
+	e.preventDefault();
 
-      controller(event.target.result);
+	if (model.expired == false) {
+		var file = e.dataTransfer.files[0],
+			reader = new FileReader();
+		reader.onload = function(event) {
+			console.log(event.target);
+			var img = document.createElement("img");
+			img.className += " draggable";
+			img.src = event.target.result;
+			holder.appendChild(img);
 
-    };
-    console.log(file);
-    reader.readAsDataURL(file);
-  }
+			controller(event.target.result);
 
-  return false;
+		};
+		console.log(file);
+		reader.readAsDataURL(file);
+	}
+
+	return false;
 };
 
-// --- DRAG 
+// --- DRAG
 interact('.draggable')
-  .draggable({
-    onmove: window.dragMoveListener,
-    restrict: {
-      restriction: 'parent',
-      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-    },
-  })
-  .resizable({
-    // resize from all edges and corners
-    edges: { left: true, right: true, bottom: true, top: true },
+	.draggable({
+		onmove: window.dragMoveListener,
+		restrict: {
+			restriction: 'parent',
+			elementRect: {
+				top: 0,
+				left: 0,
+				bottom: 1,
+				right: 1
+			}
+		},
+	})
+	.resizable({
+		// resize from all edges and corners
+		edges: {
+			left: true,
+			right: true,
+			bottom: true,
+			top: true
+		},
 
-    // keep the edges inside the parent
-    restrictEdges: {
-      outer: 'parent',
-      endOnly: true,
-    },
+		// keep the edges inside the parent
+		restrictEdges: {
+			outer: 'parent',
+			endOnly: true,
+		},
 
-    inertia: true,
-  })
-  .on('resizemove', function (event) {
-    var target = event.target,
-        x = (parseFloat(target.getAttribute('data-x')) || 0),
-        y = (parseFloat(target.getAttribute('data-y')) || 0);
+		inertia: true,
+	})
+	.on('resizemove', function(event) {
+		var target = event.target,
+			x = (parseFloat(target.getAttribute('data-x')) || 0),
+			y = (parseFloat(target.getAttribute('data-y')) || 0);
 
-    // update the element's style
-    target.style.width  = event.rect.width + 'px';
-    target.style.height = event.rect.height + 'px';
+		// update the element's style
+		target.style.width = event.rect.width + 'px';
+		target.style.height = event.rect.height + 'px';
 
-    // translate when resizing from top or left edges
-    x += event.deltaRect.left;
-    y += event.deltaRect.top;
+		// translate when resizing from top or left edges
+		x += event.deltaRect.left;
+		y += event.deltaRect.top;
 
-    target.style.webkitTransform = target.style.transform =
-        'translate(' + x + 'px,' + y + 'px)';
+		target.style.webkitTransform = target.style.transform =
+			'translate(' + x + 'px,' + y + 'px)';
 
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-    target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height);
-  });
+		target.setAttribute('data-x', x);
+		target.setAttribute('data-y', y);
+		target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height);
+	});
 
-  function dragMoveListener (event) {
-    var target = event.target,
-        // keep the dragged position in the data-x/data-y attributes
-        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+function dragMoveListener(event) {
+	var target = event.target,
+		// keep the dragged position in the data-x/data-y attributes
+		x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+		y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-    // translate the element
-    target.style.webkitTransform =
-    target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)';
+	// translate the element
+	target.style.webkitTransform =
+		target.style.transform =
+		'translate(' + x + 'px, ' + y + 'px)';
 
-    // update the posiion attributes
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-  }
-
-  // this is used later in the resizing and gesture demos
-  window.dragMoveListener = dragMoveListener;
-
-// target elements with the "draggable" class
-function drag() {
-  if (expired == true) {
-    var elems = document.querySelectorAll(".draggable");
-    [].forEach.call(elems, function(el) {
-      el.classList.remove("draggable");
-    });
-  }
+	// update the posiion attributes
+	target.setAttribute('data-x', x);
+	target.setAttribute('data-y', y);
 }
-drag();
+
+// this is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener;
