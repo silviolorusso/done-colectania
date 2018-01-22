30795428dd4b3f09b5edbbe0deb137e8a94ea47f
Stef Kors <stef.kors@gmail.com>
2018-01-22 15:12:19 +0100

    countdown and sounds

--- source/js/countdown.js
+++ source/js/countdown.js
@@ -1,8 +1,22 @@
+function loadSound () {
+  console.log('load sound!');
+  createjs.Sound.registerSound("assets/audio/beep.mp3", 'beep');
+  createjs.Sound.registerSound("assets/audio/beep-2.mp3", 'beep-2');
+  createjs.Sound.registerSound("assets/audio/ding.mp3", 'ding');
+}
+
+loadSound();
+
+function playDing () {
+  console.log('beep!');
+  createjs.Sound.play('beep');
+}
 
 
 // countdown timer
 function countdown(startTime) {
   if (startTime >= 1) {
+    createjs.Sound.play('beep-2');
     setTimeout( function(){
       startTime = startTime - 1;
       $('#countdown').html(startTime); // set current time in #countdown
@@ -14,9 +28,10 @@ function countdown(startTime) {
       $('#countdown').fadeOut(1000) // fade out the #countdown
       // TODO: start time!
     }, 200);
+    createjs.Sound.play('ding');
   }
 }
 
-var startTime = 3;
+var startTime = 4;
 countdown(startTime);
 $('#countdown').html(startTime);
