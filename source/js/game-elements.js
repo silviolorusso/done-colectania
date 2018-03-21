2405c27c1701d5ed6ec706311126b9dd403862fa
Stef Kors <stef.kors@gmail.com>
2018-03-21 11:29:03 +0100

    remove: draggable on  alert

--- source/js/game-elements.js
+++ source/js/game-elements.js
@@ -145,41 +145,12 @@ function alertMessage(message) {
     var message = messageArray[randomNum(messageArray.length)]
   }
 
-  var messageHTML = $('<div class="alert draggable"><div class="topbar draggable-handler"><span class="title">Error</span></div><img class="close closeAlert" src="/assets/img/x.png" /><h2 class="alertTitle">Alert</h2><div class="alertMessage">' + message + '</div><div class="buttons"><div class="button closeAlert">Continue</div></div></div>');
+  var messageHTML = $('<div class="alert"><div class="topbar"><span class="title">Error</span></div><img class="close closeAlert" src="/assets/img/x.png" /><h2 class="alertTitle">Alert</h2><div class="alertMessage">' + message + '</div><div class="buttons"><div class="button closeAlert">Continue</div></div></div>');
   $('body').append(messageHTML)
   messageHTML.show();
   // createjs.Sound.play("beep")
 	messageHTML.css('left', ((window.innerWidth/2) - (350/2)) +'px');
   messageHTML.css('top', (window.innerHeight/2-150) +'px');
-}
-
-var zindex = 100;
-$(document).on('mousedown', ".draggable-handler", function(e) {
-  drag = $(this).closest('.draggable')
-  drag.addClass('dragging')
-  drag.css('z-index', zindex)
-  zindex++
-  drag.css('left', e.clientX-$(this).width()/2)
-  drag.css('top', e.clientY-$(this).height()/2 - 10)
-  $(this).on('mousemove', function(e){
-    drag.css('left', e.clientX-$(this).width()/2)
-    drag.css('top', e.clientY-$(this).height()/2 - 10)
-    window.getSelection().removeAllRanges()
-  })
-});
-
-$(document).on('mouseleave', ".draggable-handler", function(e) {
-  stopDragging();
-});
-
-$(document).on('mouseup', ".draggable-handler", function(e) {
-  stopDragging();
-});
-
-function stopDragging(){
-  drag = $(this).closest('.draggable')
-  drag.removeClass('dragging')
-  $(this).off('mousemove')
 }
 
 $(document).on('click', ".closeAlert", function() {
